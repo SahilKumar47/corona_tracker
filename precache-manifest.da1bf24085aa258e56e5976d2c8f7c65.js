b1af39b491843330cdb6ba54fb7278a245f77a65
Sahil <sahilkumarpcm@gmail.com>
2020-05-05 20:27:28 +0530

    Updates

---
+++ precache-manifest.da1bf24085aa258e56e5976d2c8f7c65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1aabd224b8a74c88370c2a56fbd5db",
+    "url": "/corona_tracker/index.html"
+  },
+  {
+    "revision": "bbf90f67bf72babc0d6e",
+    "url": "/corona_tracker/static/css/main.3eefaba4.chunk.css"
+  },
+  {
+    "revision": "ced67082c5cf4dd4e69e",
+    "url": "/corona_tracker/static/js/2.bb0ae918.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona_tracker/static/js/2.bb0ae918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbf90f67bf72babc0d6e",
+    "url": "/corona_tracker/static/js/main.804237e9.chunk.js"
+  },
+  {
+    "revision": "4445a392914afdddce47",
+    "url": "/corona_tracker/static/js/runtime-main.a0cedbb3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona_tracker/static/media/covid.d7265326.png"
+  }
+]);
